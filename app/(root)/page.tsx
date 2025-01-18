682700e3934731805fad12900d927688c272636f
Iman Langaran <imanlangaran@gmail.com>
2025-01-18 17:33:35 +0330

    add base book page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 const Home = () => (
   <>
-    <Button>Click Me</Button>
+    <BookOverview />
+
+    <BookList />
   </>
 );
 
