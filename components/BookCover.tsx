01cba96391ecc41a7c7a00359d3bd7609406a1ef
Iman Langaran <imanlangaran@gmail.com>
2025-01-18 20:50:22 +0330

    some implementation

---
+++ components/BookCover.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+type BookCoverVaiant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const varientStyles: Record<BookCoverVaiant, string> = {
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
+interface Props {
+  className?: string;
+  varient?: BookCoverVaiant;
+  coverColor: string;
+  coverImage: string;
+}
+
+const BookCover = ({
+  varient = "regular",
+  className,
+  coverColor = "#012B48",
+  coverImage = "https://placehold.co/400600.png",
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        "relative transition-all duration-300",
+        varientStyles[varient]
+      )}
+    >
+      Book Side svg
+      <div
+        className=" absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <Image
+          src={coverImage}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BookCover;
