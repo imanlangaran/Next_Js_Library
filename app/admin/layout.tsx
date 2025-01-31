93731a2ce9fc5c4a436539908e5e7310d5115a23
Iman Langaran <imanlangaran@gmail.com>
2025-01-31 12:57:10 +0330

    authorization

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,11 +5,25 @@ import React, { ReactNode } from 'react'
 import "@/styles/admin.css";
 import SideBar from '@/components/admin/SideBar';
 import Header from '@/components/admin/Header';
+import { users } from '@/database/schema';
+import { eq } from 'drizzle-orm';
+import { db } from '@/database/drizzle';
 
 const Layout = async ({ children }: { children: ReactNode }) => {
 
   const session = await auth();
   if (!session?.user?.id) redirect('/sign-in');
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  console.log(isAdmin);
+
+  if (!isAdmin) redirect('/')
 
   return (
     <main className='flex min-h-screen w-full flex-row'>
