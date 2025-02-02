c5e791799bf366cd23ac5108400ecba1495f91c8
Iman Langaran <imanlangaran@gmail.com>
2025-02-02 19:34:58 +0330

    implementing borrow book

---
+++ lib/actions/book.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { db } from "@/database/drizzle";
+import dayjs from "dayjs";
+import { books } from "@/database/schema";
+import { eq } from "drizzle-orm";
+
+export const borrowBook = async (params: BorrowBookParams) => {
+  const { userId, bookId } = params;
+
+  try {
+    const book = await db
+      .select({ availableCopies: books.availableCopies })
+      .from(books)
+      .where(eq(books.id, bookId))
+      .limit(1);
+
+    if(!book.length || book[0].availableCopies <= 0) {
+      return {
+        success: false,
+        error: "Book is not available"
+      }
+    }
+
+    const dueDate = dayjs().add(7, "day").toDate().toDateString();
+
+    
+  } catch (error) {
+    console.log(error);
+
+    return {
+      success: false,
+      error: "An error accured while borrowing the book"
+    }
+  }
+}
