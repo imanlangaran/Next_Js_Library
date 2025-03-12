3416e4feb3c36f2e10286efa448360e21582e038
Iman Langaran <imanlangaran@gmail.com>
2025-03-12 14:29:57 +0330

    install scrollarea

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -12,16 +12,24 @@ const page = async () => {
   const { totalBooks, teloranceBooks } = await getBookStats();
 
   return (
-    <section className='flex flex-col gap-4'>
+    <section className='flex flex-col gap-4 h-full'>
       <div className='grid grid-cols-3 gap-4'>
         <StatsCard title="Borrowed books" telorance={teloranceBorrowRequest} value={totalBorrowRequest} />
         <StatsCard title="Total Users" telorance={teloranceUsers} value={totalUsers} />
         <StatsCard title="Total Books" telorance={teloranceBooks} value={totalBooks} />
       </div>
-      <div className='grid grid-cols-2 gap-4'>
-        <OverviewCard title="Borrow Requests" bottonLink="/admin/borrow-records" />
-        <OverviewCard title="Account Requests" bottonLink="/admin/users" className="row-span-2" />
-        <OverviewCard title="Recently Added Books" bottonLink="/admin/books" />
+      <div className='grid grid-cols-2 gap-4 h-full'>
+        <OverviewCard title="Borrow Requests" bottonLink="/admin/borrow-records">
+        <ScrollArea>
+          <>the table</>
+        </ScrollArea>
+        </OverviewCard>
+        <OverviewCard title="Account Requests" bottonLink="/admin/users" className="row-span-2">
+        <>the table</>
+        </OverviewCard>
+        <OverviewCard title="Recently Added Books" bottonLink="/admin/books">
+        <>the table</>
+        </OverviewCard>
       </div>
     </section>
   )
--- components/admin/OverviewCard.tsx
+++ components/admin/OverviewCard.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
+import { Button } from '../ui/button';
 
 interface Props {
   title: string;
   bottonLink: string;
   className?: string;
+  children: React.ReactNode;
 }
 
-const OverviewCard = ({ title, bottonLink, className }: Props) => {
+const OverviewCard = ({ title, bottonLink, className, children }: Props) => {
   return (
     <div className={cn('admin-main p-4 h-full', className)}>
-      <h3 className='text-lg font-medium text-dark-500'>{title}</h3>
-      <Link href={bottonLink} className='text-sm text-blue-500'>View all</Link>
+      <div className='flex items-center justify-between'>
+        <h3 className='text-lg font-medium text-dark-500'>{title}</h3>
+        <Button variant="outline" size="sm" asChild className='view-btn'>
+          <Link href={bottonLink} className='text-base text-primary-admin'>View all</Link>
+        </Button>
+      </div>
+      {children}
     </div>
   )
 }
