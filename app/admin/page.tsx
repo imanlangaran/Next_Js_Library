cf024fbcb65d3622c885b01b59150bac946ad6c1
Iman Langaran <imanlangaran@gmail.com>
2025-03-12 15:46:27 +0330

    just

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,20 @@
 import StatsCard from '@/components/admin/StatsCard'
 import React from 'react'
-import { getBorrowRequestStats } from '@/lib/admin/actions/borrow-records'
+import { getBorrowRecords, getBorrowRequestStats } from '@/lib/admin/actions/borrow-records'
 import { getUserStats } from '@/lib/admin/actions/user'
 import { getBookStats } from '@/lib/admin/actions/book'
 import OverviewCard from '@/components/admin/OverviewCard'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { getBooks } from '@/lib/admin/actions/book'
 
 const page = async () => {
 
   const { totalBorrowRequest, teloranceBorrowRequest } = await getBorrowRequestStats();
   const { totalUsers, teloranceUsers } = await getUserStats();
   const { totalBooks, teloranceBooks } = await getBookStats();
+
+  const borrowRecords = await getBorrowRecords();
+  const { data: books } = await getBooks();
 
   return (
     <section className='flex flex-col gap-4 h-full'>
@@ -22,14 +26,26 @@ const page = async () => {
       <div className='grid grid-cols-2 gap-4 h-full'>
         <OverviewCard title="Borrow Requests" bottonLink="/admin/borrow-records">
         <ScrollArea>
-          <>the table</>
+          {borrowRecords.map((record) => (
+            <div key={record.id}>
+              {record.id}
+            </div>
+          ))}
         </ScrollArea>
         </OverviewCard>
         <OverviewCard title="Account Requests" bottonLink="/admin/users" className="row-span-2">
         <>the table</>
         </OverviewCard>
         <OverviewCard title="Recently Added Books" bottonLink="/admin/books">
-        <>the table</>
+          <ScrollArea className='bg-red-500 max-h-full'>
+            <div className='flex flex-col gap-4 max-h-full'>
+              {books?.map((book) => (
+                <div key={book.id}>
+                  {book.title}
+                </div>
+              ))}
+            </div>
+          </ScrollArea>
         </OverviewCard>
       </div>
     </section>
--- components/admin/OverviewCard.tsx
+++ components/admin/OverviewCard.tsx
@@ -12,14 +12,16 @@ interface Props {
 
 const OverviewCard = ({ title, bottonLink, className, children }: Props) => {
   return (
-    <div className={cn('admin-main p-4 h-full', className)}>
-      <div className='flex items-center justify-between'>
+    <div className={cn('admin-main p-4 bg-slate-200 flex flex-col h-full', className)}>
+      <div className='flex items-center justify-between h-fit bg-slate-300'>
         <h3 className='text-lg font-medium text-dark-500'>{title}</h3>
         <Button variant="outline" size="sm" asChild className='view-btn'>
           <Link href={bottonLink} className='text-base text-primary-admin'>View all</Link>
         </Button>
       </div>
-      {children}
+      <div className='bg-slate-400 flex-1 max-h-full'>
+        {children}
+      </div>
     </div>
   )
 }
